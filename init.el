ac5742755769c87ec87a4a61eee970c52b999a97
Chris Peterson <peterson.m.chris@gmail.com>
2017-05-07 11:10:28 -0500

    Add ag search

--- init.el
+++ init.el
@@ -8,13 +8,16 @@
     ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(package-selected-packages
    (quote
-    (grizzl projectile web-mode ace-jump-mode color-theme-sanityinc-tomorrow))))
+    (ag grizzl projectile web-mode ace-jump-mode color-theme-sanityinc-tomorrow))))
 
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(when (string= system-type "darwin")
+  (setq dired-use-ls-dired nil))
 
 (package-initialize)
 (unless package-archive-contents
@@ -32,12 +35,18 @@
 (setq auto-save-default nil)
 (setq-default indent-tabs-mode nil)
 
+(add-to-list 'load-path "/path/to/ag.el")
+(require 'ag)
+
 (require 'web-mode)
 (setq css-indent-offset 2)
 (setq js-indent-level 2)
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+(global-set-key (kbd "C-x g") 'magit-status)
 
 (projectile-global-mode)
 (setq projectile-completion-system 'grizzl)
@@ -51,6 +60,7 @@
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 (global-set-key (kbd "M-p") 'ace-window)
+(global-set-key (kbd "C-x p") 'ag-project)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
