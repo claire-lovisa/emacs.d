9f3cffac4a987713bf337277fa78072356d994b7
Chris Peterson <peterson.m.chris@gmail.com>
2017-05-22 08:35:44 -0500

    Update

--- init.el
+++ init.el
@@ -48,8 +48,10 @@
 (require 'web-mode)
 (setq css-indent-offset 2)
 (setq js-indent-level 2)
+
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
+
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
