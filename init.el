cc0111b7ad758f4dfea231c7d5436650a5a3ebe8
Chris Peterson <peterson.m.chris@gmail.com>
2017-08-20 01:33:52 -0400

    Multiple cursors

--- init.el
+++ init.el
@@ -3,9 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (flatland)))
  '(custom-safe-themes
    (quote
-    ("a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(package-selected-packages
    (quote
     (ace-mc flymake-json flatland-theme multiple-cursors less-css-mode magit ag grizzl projectile web-mode ace-jump-mode color-theme-sanityinc-tomorrow))))
@@ -84,10 +88,9 @@
 (global-set-key (kbd "C-x j") 'ace-mc-add-single-cursor)
 
 (global-set-key (kbd "C-c j v") 'flymake-json-load)
-
-;;(global-set-key (kbd "C-S->") 'mc/mark-next-like-this)
-;;(global-set-key (kbd "C-S-<") 'mc/mark-previous-like-this)
-;;(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-u") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-j") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-x i") 'mc/mark-all-like-this)
 
 (setq-default message-log-max nil)
 (kill-buffer "*Messages*")
